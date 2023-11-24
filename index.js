3909deb52e424cd2b4bb42cec559312debadef8e
Abubakar Lawwal <40341812+Abumib@users.noreply.github.com>
2023-11-24 10:56:36 +0100

    problem is it fetches each title more than once.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
 
-app.listen(5000, () => { console.log("Server started on port 5000") });
+
 
 app.get("/api", (req,res) => {
     res.json({"users": ["userOne","userTwo","userThree"]});
@@ -11,18 +11,35 @@ app.get("/api", (req,res) => {
 app.get("/events", (req, res) => {
     (async () => {
         try {
-            const browser = await puppeteer.launch({ headless: true });
+            const browser = await puppeteer.launch({
+                headless: false
+            }); 
 
             const page = await browser.newPage();
             await page.goto(`https://www.eventbrite.co.uk/o/rise-london-16793628230`);
-            
-            const grabTitle = await page.evaluate(() => {
-                const eventTitle = document.querySelector(".card-text--truncated__one")
-                return eventTitle.innerText;
-            })
+
+            //$$eval is a puppeteer function you can check the documentation
+            const grabTitle = await page.$$eval('[data-testid="organizer-profile__future-events"] .eds-event-card__formatted-name--is-clamped', titles => {
+                console.log("Here are the titles: " + titles)
+                //return titles.textcontent
+                return titles.map(title => title.textContent);
+              });
+             
+            ///////////////////////////////////I was testing something below. You can delete if you want.//////////////////////////////////////
+            // const grabTitle = await page.evaluate(() => {
+            //     const eventTitles = document.querySelectorAll('[data-testid="organizer-profile__future-events"] .eds-event-card__formatted-name--is-clamped');
+                
+            //     // Convert NodeList to an array and then map to extract the innerText of each element
+            //     const titleArray = Array.from(eventTitles).map(title => title.innerText);
+              
+            //     console.log("These are the titles: ", titleArray);
+              
+            //     return titleArray[0];
+            //   });
+
             console.log(grabTitle);
             res.send(grabTitle)
-            await browser.close() 
+            // await browser.close() 
         } catch (err) {
             console.log(err)
             res.send([])
@@ -30,4 +47,6 @@ app.get("/events", (req, res) => {
     })();
 });
 
+
+app.listen(5000, () => { console.log("Server started on port 5000") });
 
